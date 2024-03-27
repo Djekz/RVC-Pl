18fbb5331bca2c19d211e7d7183d2f6d06efe6a7
Blane <161300198+Blane187@users.noreply.github.com>
2024-03-28 06:06:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -332,6 +332,6 @@ with gr.Blocks(theme='Hev832/Pl-tme') as app:
         audio_player = gr.Audio()
         inputs = [audio_picker,model_picker,index_picker,index_rate,pitch,method,output_name]
         audio_picker.change(fn=update_audio_player, inputs=[audio_picker],outputs=[audio_player])
-        convert_button.click(convert, inputs=inputs,outputs=[audio_picker,audio_player,output_name])
+        convert_button.click(convert, inputs=inputs,outputs=[audio_picker,audio_player])
 
 app.launch(debug=True, share=True)
